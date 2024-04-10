4fd6e588ac7f69a9c9c4a5dcf9baa1eb77b9f15f
hdavidpb <68169674+hdavidpb@users.noreply.github.com>
2024-04-10 15:46:15 -0500

    Update ProjectCard.tsx

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -8,7 +8,7 @@ interface Props{
 export const ProjectCard = ({project}:Props) => {
   return (
     <div className="shadow-xl md:h-[340px] w-[96%] md:w-[750px] bg-white flex md:flex-row flex-col justify-center items-center rounded-xl  mt-6 gap-3">
-    <div className="flex-1 flex flex-col justify-start items-start gap-6 h-full pt-16">
+    <div className="flex-1 flex flex-col justify-start items-start gap-6 h-full pt-16 w-full ">
       <div className="w-full flex justify-start items-center h-[50px]">
         <div className="w-2 h-full bg-primary"></div>
         <div className="w-full flex flex-col justify-between items-start pl-7">

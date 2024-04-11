2163c184037d7805e6c3bb63789f2813cad14634
hdavidpb <68169674+hdavidpb@users.noreply.github.com>
2024-04-11 15:59:40 -0500

    ✨ADD:Spinner agenda

--- src/app/agendar-consulta/page.tsx
+++ src/app/agendar-consulta/page.tsx
@@ -1,18 +1,15 @@
-
+import { AgendarConsultaFrame } from "@/components/AgendarConsultaFrame";
 import { ISection } from "@/interfaces/sections.interface";
 import { Metadata } from "next";
 
-export const metadata:Metadata ={
-  title:"Claudia Arango | Agendar consulta"
-}
+export const metadata: Metadata = {
+  title: "Claudia Arango | Agendar consulta",
+};
 
-
-
-export default async function CurriculemPage  ()  {
+export default async function CurriculemPage() {
   return (
     <div className="w-full  flex flex-col justify-start items-center  bg-[#fbfcfd] py-4">
-           <iframe className="w-full h-screen" src="https://calendly.com/claudia-arangonutricionista/consulta-1-vez" name="Consulta de primera vez con Claudia Arango - Nutricionista." />
-
+      <AgendarConsultaFrame />
     </div>
   );
-};
+}

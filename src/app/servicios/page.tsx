069d070717fe02da09f0e6360f5ab08ee789d6fa
hdavidpb <68169674+hdavidpb@users.noreply.github.com>
2024-04-17 02:56:27 -0500

    Update page.tsx

--- src/app/servicios/page.tsx
+++ src/app/servicios/page.tsx
@@ -1,21 +1,21 @@
-import { ServiceCard } from "@/components/services/ServiceCard";
-import { ServicesResponse } from "@/interfaces/services.interface";
+// import { ServiceCard } from "@/components/services/ServiceCard";
+// import { ServicesResponse } from "@/interfaces/services.interface";
 
 
-async function getServices (){
-    const API_URL = process.env.BACKEND_URL
-    const services:ServicesResponse[] =  await fetch(`${API_URL}/api/services`,{next:{revalidate:60}}).then(response => response.json())
+// async function getServices (){
+//     const API_URL = process.env.BACKEND_URL
+//     const services:ServicesResponse[] =  await fetch(`${API_URL}/api/services`,{next:{revalidate:60}}).then(response => response.json())
 
-    return services 
-}
+//     return services 
+// }
 
 export default async function ServicesPage () {
-    const services = await getServices()
+    // const services = await getServices()
     return (
         <section className="w-full h-[80dvh]  flex flex-col justify-center items-center  bg-secondary py-4">
-            {services.map(service => (
+            {/* {services.map(service => (
                 <ServiceCard key={service.consultType} consultType={service.consultType} description={service.description} price={service.price}/>
-            ))}
+            ))} */}
         </section>
     )
 }
